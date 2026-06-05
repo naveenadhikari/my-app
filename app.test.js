59c0536103d2ddc01c407095f1ee9bc3026e5b4f
Naveen <naveenadhikari011@gmail.com>
2026-06-05 14:16:16 +0530

    inti edit

--- app.test.js
+++ app.test.js
@@ -4,7 +4,7 @@ const app = require('./app');
 test('GET / returns hello message', async () => {
   const res = await request(app).get('/');
   expect(res.statusCode).toBe(200);
-  expect(res.body.message).toBe('Hello from proxima1');
+  expect(res.body.message).toBe('Hello from proxima1213');
 });
 
 test('GET /health returns ok', async () => {
