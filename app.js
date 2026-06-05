644547ec0a2e1fd588fd386dcc953ff6b83a0ee7
Naveen <naveenadhikari011@gmail.com>
2026-06-05 14:55:35 +0530

    inti edit2311

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello from proxima211' });
+  res.json({ message: 'Hello from proxima2121' });
    
 });
 
